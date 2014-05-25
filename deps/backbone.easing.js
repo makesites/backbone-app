d1a4d633bf783ecc354d4a9058c7db7a6573a3a3
Makis Tracend <makis.tracend@gmail.com>
2014-05-24 22:55:45 -0700

    #80 normalizing build

--- deps/backbone.easing.js
+++ deps/backbone.easing.js
@@ -1,18 +1,24 @@
-
-/* Backbone Easing
- * Source: https://github.com/makesites/backbone-easing
- * Copyright © Makesites.org
+/**
+ * @name backbone.easing
+ * 
  *
- * Initiated by Makis Tracend (@tracend)
- * Distributed through [Makesites.org](http://makesites.org)
- * Released under the [MIT license](http://makesites.org/licenses/MIT)
+ * Version: 0.1.0 (Sun, 25 May 2014 05:51:42 GMT)
+ * Source: http://github.com/makesites/backbone-easing
+ *
+ * @author makesites
+ * Initiated by: Makis Tracend (@tracend)
+ *
+ * @cc_on Copyright © Makesites.org
+ * @license MIT license
  */
 
-(function(w, $, _, Backbone, APP) {
+(function(window, $, _, Backbone, APP) {
 
 	// support for Backbone APP() view if available...
 	var isAPP = ( typeof APP !== "undefined" && typeof APP.View !== "undefined" );
 	var View = ( isAPP ) ? APP.View : Backbone.View;
+
+
 
 
 	var Easing = View.extend({
@@ -119,18 +125,311 @@ _.mixin({
 	}
 });
 
+// --------------------------------------------------
 // easing.js v0.5.4
 // Generic set of easing functions with AMD support
 // https://github.com/danro/easing-js
-(function(a,b){typeof define=="function"?define(b):typeof module!="undefined"?module.exports=b:this[a]=b})("easing",{easeInQuad:function(a){return Math.pow(a,2)},easeOutQuad:function(a){return-(Math.pow(a-1,2)-1)},easeInOutQuad:function(a){return(a/=.5)<1?.5*Math.pow(a,2):-0.5*((a-=2)*a-2)},easeInCubic:function(a){return Math.pow(a,3)},easeOutCubic:function(a){return Math.pow(a-1,3)+1},easeInOutCubic:function(a){return(a/=.5)<1?.5*Math.pow(a,3):.5*(Math.pow(a-2,3)+2)},easeInQuart:function(a){return Math.pow(a,4)},easeOutQuart:function(a){return-(Math.pow(a-1,4)-1)},easeInOutQuart:function(a){return(a/=.5)<1?.5*Math.pow(a,4):-0.5*((a-=2)*Math.pow(a,3)-2)},easeInQuint:function(a){return Math.pow(a,5)},easeOutQuint:function(a){return Math.pow(a-1,5)+1},easeInOutQuint:function(a){return(a/=.5)<1?.5*Math.pow(a,5):.5*(Math.pow(a-2,5)+2)},easeInSine:function(a){return-Math.cos(a*(Math.PI/2))+1},easeOutSine:function(a){return Math.sin(a*(Math.PI/2))},easeInOutSine:function(a){return-0.5*(Math.cos(Math.PI*a)-1)},easeInExpo:function(a){return a===0?0:Math.pow(2,10*(a-1))},easeOutExpo:function(a){return a===1?1:-Math.pow(2,-10*a)+1},easeInOutExpo:function(a){return a===0?0:a===1?1:(a/=.5)<1?.5*Math.pow(2,10*(a-1)):.5*(-Math.pow(2,-10*--a)+2)},easeInCirc:function(a){return-(Math.sqrt(1-a*a)-1)},easeOutCirc:function(a){return Math.sqrt(1-Math.pow(a-1,2))},easeInOutCirc:function(a){return(a/=.5)<1?-0.5*(Math.sqrt(1-a*a)-1):.5*(Math.sqrt(1-(a-=2)*a)+1)},easeOutBounce:function(a){return a<1/2.75?7.5625*a*a:a<2/2.75?7.5625*(a-=1.5/2.75)*a+.75:a<2.5/2.75?7.5625*(a-=2.25/2.75)*a+.9375:7.5625*(a-=2.625/2.75)*a+.984375},easeInBack:function(a){var b=1.70158;return a*a*((b+1)*a-b)},easeOutBack:function(a){var b=1.70158;return(a-=1)*a*((b+1)*a+b)+1},easeInOutBack:function(a){var b=1.70158;return(a/=.5)<1?.5*a*a*(((b*=1.525)+1)*a-b):.5*((a-=2)*a*(((b*=1.525)+1)*a+b)+2)},elastic:function(a){return-1*Math.pow(4,-8*a)*Math.sin((a*6-1)*2*Math.PI/2)+1},swingFromTo:function(a){var b=1.70158;return(a/=.5)<1?.5*a*a*(((b*=1.525)+1)*a-b):.5*((a-=2)*a*(((b*=1.525)+1)*a+b)+2)},swingFrom:function(a){var b=1.70158;return a*a*((b+1)*a-b)},swingTo:function(a){var b=1.70158;return(a-=1)*a*((b+1)*a+b)+1},bounce:function(a){return a<1/2.75?7.5625*a*a:a<2/2.75?7.5625*(a-=1.5/2.75)*a+.75:a<2.5/2.75?7.5625*(a-=2.25/2.75)*a+.9375:7.5625*(a-=2.625/2.75)*a+.984375},bouncePast:function(a){return a<1/2.75?7.5625*a*a:a<2/2.75?2-(7.5625*(a-=1.5/2.75)*a+.75):a<2.5/2.75?2-(7.5625*(a-=2.25/2.75)*a+.9375):2-(7.5625*(a-=2.625/2.75)*a+.984375)},easeFromTo:function(a){return(a/=.5)<1?.5*Math.pow(a,4):-0.5*((a-=2)*Math.pow(a,3)-2)},easeFrom:function(a){return Math.pow(a,4)},easeTo:function(a){return Math.pow(a,.25)}});
+// This code may be freely distributed under the MIT license
+// http://danro.mit-license.org/
+// --------------------------------------------------
+// All functions adapted from Thomas Fuchs & Jeremy Kahn
+// Easing Equations (c) 2003 Robert Penner, BSD license
+// https://raw.github.com/danro/easing-js/master/LICENSE
+// --------------------------------------------------
+(function (name, definition) {
+  /*global define module*/
+  if (typeof define == 'function') define(definition);
+  else if (typeof module != 'undefined') module.exports = definition;
+  else this[name] = definition;
+}('easing', {
+  easeInQuad: function(pos) {
+    return Math.pow(pos, 2);
+  },
 
+  easeOutQuad: function(pos) {
+    return -(Math.pow((pos-1), 2) -1);
+  },
 
+  easeInOutQuad: function(pos) {
+    if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,2);
+    return -0.5 * ((pos-=2)*pos - 2);
+  },
+
+  easeInCubic: function(pos) {
+    return Math.pow(pos, 3);
+  },
+
+  easeOutCubic: function(pos) {
+    return (Math.pow((pos-1), 3) +1);
+  },
+
+  easeInOutCubic: function(pos) {
+    if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,3);
+    return 0.5 * (Math.pow((pos-2),3) + 2);
+  },
+
+  easeInQuart: function(pos) {
+    return Math.pow(pos, 4);
+  },
+
+  easeOutQuart: function(pos) {
+    return -(Math.pow((pos-1), 4) -1);
+  },
+
+  easeInOutQuart: function(pos) {
+    if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,4);
+    return -0.5 * ((pos-=2)*Math.pow(pos,3) - 2);
+  },
+
+  easeInQuint: function(pos) {
+    return Math.pow(pos, 5);
+  },
+
+  easeOutQuint: function(pos) {
+    return (Math.pow((pos-1), 5) +1);
+  },
+
+  easeInOutQuint: function(pos) {
+    if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,5);
+    return 0.5 * (Math.pow((pos-2),5) + 2);
+  },
+
+  easeInSine: function(pos) {
+    return -Math.cos(pos * (Math.PI/2)) + 1;
+  },
+
+  easeOutSine: function(pos) {
+    return Math.sin(pos * (Math.PI/2));
+  },
+
+  easeInOutSine: function(pos) {
+    return (-0.5 * (Math.cos(Math.PI*pos) -1));
+  },
+
+  easeInExpo: function(pos) {
+    return (pos===0) ? 0 : Math.pow(2, 10 * (pos - 1));
+  },
+
+  easeOutExpo: function(pos) {
+    return (pos===1) ? 1 : -Math.pow(2, -10 * pos) + 1;
+  },
+
+  easeInOutExpo: function(pos) {
+    if(pos===0) return 0;
+    if(pos===1) return 1;
+    if((pos/=0.5) < 1) return 0.5 * Math.pow(2,10 * (pos-1));
+    return 0.5 * (-Math.pow(2, -10 * --pos) + 2);
+  },
+
+  easeInCirc: function(pos) {
+    return -(Math.sqrt(1 - (pos*pos)) - 1);
+  },
+
+  easeOutCirc: function(pos) {
+    return Math.sqrt(1 - Math.pow((pos-1), 2));
+  },
+
+  easeInOutCirc: function(pos) {
+    if((pos/=0.5) < 1) return -0.5 * (Math.sqrt(1 - pos*pos) - 1);
+    return 0.5 * (Math.sqrt(1 - (pos-=2)*pos) + 1);
+  },
+
+  easeOutBounce: function(pos) {
+    if ((pos) < (1/2.75)) {
+      return (7.5625*pos*pos);
+    } else if (pos < (2/2.75)) {
+      return (7.5625*(pos-=(1.5/2.75))*pos + 0.75);
+    } else if (pos < (2.5/2.75)) {
+      return (7.5625*(pos-=(2.25/2.75))*pos + 0.9375);
+    } else {
+      return (7.5625*(pos-=(2.625/2.75))*pos + 0.984375);
+    }
+  },
+
+  easeInBack: function(pos) {
+    var s = 1.70158;
+    return (pos)*pos*((s+1)*pos - s);
+  },
+
+  easeOutBack: function(pos) {
+    var s = 1.70158;
+    return (pos=pos-1)*pos*((s+1)*pos + s) + 1;
+  },
+
+  easeInOutBack: function(pos) {
+    var s = 1.70158;
+    if((pos/=0.5) < 1) return 0.5*(pos*pos*(((s*=(1.525))+1)*pos -s));
+    return 0.5*((pos-=2)*pos*(((s*=(1.525))+1)*pos +s) +2);
+  },
+
+  elastic: function(pos) {
+    return -1 * Math.pow(4,-8*pos) * Math.sin((pos*6-1)*(2*Math.PI)/2) + 1;
+  },
+
+  swingFromTo: function(pos) {
+    var s = 1.70158;
+    return ((pos/=0.5) < 1) ? 0.5*(pos*pos*(((s*=(1.525))+1)*pos - s)) :
+    0.5*((pos-=2)*pos*(((s*=(1.525))+1)*pos + s) + 2);
+  },
+
+  swingFrom: function(pos) {
+    var s = 1.70158;
+    return pos*pos*((s+1)*pos - s);
+  },
+
+  swingTo: function(pos) {
+    var s = 1.70158;
+    return (pos-=1)*pos*((s+1)*pos + s) + 1;
+  },
+
+  bounce: function(pos) {
+    if (pos < (1/2.75)) {
+      return (7.5625*pos*pos);
+    } else if (pos < (2/2.75)) {
+      return (7.5625*(pos-=(1.5/2.75))*pos + 0.75);
+    } else if (pos < (2.5/2.75)) {
+      return (7.5625*(pos-=(2.25/2.75))*pos + 0.9375);
+    } else {
+      return (7.5625*(pos-=(2.625/2.75))*pos + 0.984375);
+    }
+  },
+
+  bouncePast: function(pos) {
+    if (pos < (1/2.75)) {
+      return (7.5625*pos*pos);
+    } else if (pos < (2/2.75)) {
+      return 2 - (7.5625*(pos-=(1.5/2.75))*pos + 0.75);
+    } else if (pos < (2.5/2.75)) {
+      return 2 - (7.5625*(pos-=(2.25/2.75))*pos + 0.9375);
+    } else {
+      return 2 - (7.5625*(pos-=(2.625/2.75))*pos + 0.984375);
+    }
+  },
+
+  easeFromTo: function(pos) {
+    if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,4);
+    return -0.5 * ((pos-=2)*Math.pow(pos,3) - 2);
+  },
+
+  easeFrom: function(pos) {
+    return Math.pow(pos,4);
+  },
+
+  easeTo: function(pos) {
+    return Math.pow(pos,0.25);
+  }
+}));
 
 /* Tick.js
  * Source: https://github.com/makesites/tick
  * Copyright © Makesites.org
+ *
+ * Initiated by Makis Tracend (@tracend)
+ * Distributed through [Makesites.org](http://makesites.org)
+ * Released under the [MIT license](http://makesites.org/licenses/MIT)
  */
-(function(e){var t=function(e){e=e||{};if(e.rate)this.options.rate=e.rate;this.rate();this.loop()};t.prototype={options:{rate:1e3/60},queue:[],rate:function(t){t=t||this.options.rate;e.requestAnimFrame=e.requestAnimFrame||function(n){return e.requestAnimationFrame||e.webkitRequestAnimationFrame||e.mozRequestAnimationFrame||e.oRequestAnimationFrame||e.msRequestAnimationFrame||function(n){e.setTimeout(function(){n(+(new Date))},t)}}()},loop:function(t){this.process(t);e.requestAnimFrame(this.loop.bind(this))},process:function(e){for(var t in this.queue){var n=this.queue[t];if(!(e%n.interval)||n.run+n.interval>e||typeof n.fn!=="function")continue;n.fn();if(this.queue[t])this.queue[t].run=e}},add:function(e,t){if(typeof e!=="function")return;t=t||this.options.rate;var n={fn:e,interval:t,run:0};this.queue.push(n)},remove:function(e){var t=false;for(var n in this.queue){var r=this.queue[n];if(String(r.fn)===String(e)){t=true;delete this.queue[n]}}return t}};e.Tick=t})(this.window)
+
+(function( window ){
+
+var Tick = function( options ){
+	// merge options
+	options = options || {};
+	if(options.rate) this.options.rate = options.rate;
+	// setup animation rate
+	this.rate();
+	// start loop
+	this.loop();
+};
+
+Tick.prototype = {
+
+	options: {
+		rate: 1000 / 60 // standard 60fps
+	},
+
+	queue: [],
+
+	rate: function( rate ){
+		rate = rate || this.options.rate;
+		// RequestAnimationFrame shim - Source: http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+		window.requestAnimFrame = window.requestAnimFrame || ( function( callback ) {
+			return window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function ( callback ) {
+				window.setTimeout( function () {
+					callback(+new Date());
+				}, rate );
+			};
+		})();
+
+	},
+
+	loop: function( timestamp ){
+
+		// feature-detect if rAF and now() are of the same scale (epoch or high-res),
+		// if not, we have to do a timestamp fix on each frame
+		//Source: https://github.com/ded/morpheus/blob/master/src/morpheus.js#L91
+		//if (fixTs) timestamp = now();
+
+		this.process( timestamp );
+		// reset loop
+		window.requestAnimFrame(this.loop.bind(this)); // bind only works in > ES5
+	},
+
+	process: function( timestamp ){
+		// loop through queue
+		for( var i in this.queue ){
+			var item = this.queue[i];
+			// prerequisite
+			if( typeof item.fn !== "function") continue;
+			// restrict execution if not time yet
+			var step = (timestamp % item.interval);
+			if( step === 0 || item.run + item.interval > timestamp) continue;
+			// run
+			item.fn(); // context?
+			// record last run
+			// condition in case the item was released in the meantime...
+			if( this.queue[i] ) this.queue[i].run = timestamp;
+		}
+	},
+
+	// interface
+
+	add: function( fn, interval ){
+		// prerequisite
+		if( typeof fn !== "function" ) return;
+		// fallback
+		interval = interval || this.options.rate;
+		var item = {
+			fn: fn,
+			interval: interval,
+			run: 0
+		};
+		this.queue.push( item );
+	},
+
+	remove: function( fn ){
+		var exists = false;
+		for( var i in this.queue ){
+			var item = this.queue[i];
+			if( String(item.fn) === String(fn) ){
+				exists = true;
+				delete this.queue[i];
+			}
+		}
+		return exists;
+	}
+
+};
+
+// save to the global namespace
+window.Tick = Tick;
+
+})( this.window );
+
 
 
 
