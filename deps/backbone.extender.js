d5bbf5055f3ffac7130fa67e39504f10f317e0d1
Makis Tracend <makis.tracend@gmail.com>
2016-12-07 05:05:31 -0800

    :up: backbone.extender

--- deps/backbone.extender.js
+++ deps/backbone.extender.js
@@ -9,7 +9,7 @@
 	var extend = function(protoProps, staticProps) {
 
 		var parent = this;
-
+		var child;
 
 		if (protoProps){
 			_.each(protoProps, function(value, key){
@@ -22,7 +22,37 @@
 			});
 		}
 
-		return origExtend.call(this, protoProps, staticProps);
+		// FIX: can't use original .extend to contain child prototype
+		//return origExtend.call(this, protoProps, staticProps);
+
+		// The constructor function for the new subclass is either defined by you
+		// (the "constructor" property in your `extend` definition), or defaulted
+		// by us to simply call the parent's constructor.
+		if (protoProps && _.has(protoProps, 'constructor')) {
+			child = protoProps.constructor;
+		} else {
+			child = function(){ return parent.apply(this, arguments); };
+		}
+
+		// Add static properties to the constructor function, if supplied.
+		_.extend(child, parent, staticProps);
+
+		// Set the prototype chain to inherit from `parent`, without calling
+		// `parent`'s constructor function.
+		var Surrogate = function(){ this.constructor = child; };
+		Surrogate.prototype = parent.prototype;
+		child.prototype = Object.create(new Surrogate());
+
+		// Add prototype properties (instance properties) to the subclass,
+		// if supplied.
+		if (protoProps) child.prototype = _.extend({}, child.prototype, protoProps);
+
+		// Set a convenience property in case the parent's prototype is needed
+		// later.
+		child.__super__ = parent.prototype;
+
+		return child;
+
 	};
 
 	// Set up inheritance for the model, collection, router, view and history.
